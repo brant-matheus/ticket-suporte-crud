8cf805a47caba44c01de72304cdc944c0066c625
brant <brantccm@Hotmail.com>
2024-05-14 11:13:12 -0300

    visualize user created at and updated *formated pt-BR* at throughout tooltip eye icon

--- front-end/app/suporte/gestao-usuarios/columns.tsx
+++ front-end/app/suporte/gestao-usuarios/columns.tsx
@@ -3,15 +3,23 @@
 import { ColumnDef } from "@tanstack/react-table";
 import DeleteDialog from "@/components/buttons/delete-dialog";
 import EditButton from "./edit-button";
-import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+import { ArrowUpDown, MoreHorizontal, Eye, Ghost } from "lucide-react";
 import { Button } from "@/components/ui/button";
-
+import { DateTime } from "luxon";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 // This type is used to define the shape of our data.
 export type TData = {
   id: number;
   fullName: string;
   email: string;
   isAdmin: number;
+  createdAt: string;
+  updatedAt: string;
 };
 
 export const columns: ColumnDef<TData>[] = [
@@ -31,7 +39,6 @@ export const columns: ColumnDef<TData>[] = [
     accessorKey: "isAdmin",
     // remove this, use it on email
     header: "permissão",
-
     cell: ({ row }) => {
       const user = row.original;
       var permission = "administrador";
@@ -40,6 +47,43 @@ export const columns: ColumnDef<TData>[] = [
       }
 
       return <p>{permission}</p>;
+    },
+  },
+  {
+    accessorKey: "createdAt",
+    header: "modificado",
+    cell: ({ row }) => {
+      const user = row.original;
+      //user create at date time
+      const createdAt = DateTime.fromISO(user.createdAt, {
+        locale: "pt-BR",
+      }).toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS);
+
+      // user update at date time
+      const updateAt = DateTime.fromISO(user.updatedAt, {
+        locale: "pt-BR",
+      }).toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS);
+      return (
+        <TooltipProvider delayDuration={0}>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button variant="ghost">
+                <Eye />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <div className="">
+                <p>usuário criado em: {createdAt}</p>
+                {updateAt === createdAt ? (
+                  <p>usuário ainda não foi editado</p>
+                ) : (
+                  <p>usuário editado em: {updateAt}</p>
+                )}
+              </div>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      );
     },
   },
   {
